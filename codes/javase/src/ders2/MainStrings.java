40cbc39d6920a52f26ae492d781d89b3bed606f1
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2023-10-09 20:56:53 +0400

    string

--- codes/javase/src/ders2/MainStrings.java
+++ codes/javase/src/ders2/MainStrings.java
@@ -4,7 +4,10 @@ public class MainStrings {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("salam");
+String name ="Aygun";
+String surname= "Memmedzade";
+String fullname=name+" "+surname;
+System.out.println("Menim tam adim:"+" "+fullname);
 	}
 
 }
