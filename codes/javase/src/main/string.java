81c10503966dc77d020cfd923d055ef99932d038
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2023-10-12 19:33:43 +0400

    string

---
+++ codes/javase/src/main/string.java
@@ -0,0 +1,11 @@
+package main;
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String Face="Facebook";
+		System.out.println(Face);
+	}
+
+}
