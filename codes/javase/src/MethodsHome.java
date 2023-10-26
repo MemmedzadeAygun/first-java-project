e303b00775c6c8630a679c71c5657460234a8883
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2023-10-26 15:59:58 +0400

    Methods

---
+++ codes/javase/src/MethodsHome.java
@@ -0,0 +1,22 @@
+
+public class MethodsHome {
+	
+	public static void printNumbers(int begin,int end){
+		
+		System.out.println("ededler:"+begin+" "+end);
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		printNumbers(20,100);
+		
+		MethodsHome method=new MethodsHome();
+		method.printNumbers(10,30);
+		method.printNumbers(40,50);
+		method.printNumbers(60,70);
+						    
+	}
+
+}
