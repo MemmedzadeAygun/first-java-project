006aaec02fc1a4bc0585e311608c8cedf946193a
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2024-01-04 14:00:03 +0400

    Spring web

--- codes/spring/spring-java13/spring-java13/src/main/java/az/developia/springjava13/SpringJava13Application.java
+++ codes/spring/spring-java13/spring-java13/src/main/java/az/developia/springjava13/SpringJava13Application.java
@@ -14,17 +14,17 @@ public class SpringJava13Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext a =SpringApplication.run(SpringJava13Application.class, args);
 		
-		Person personBean= a.getBean("myPerson1",Person.class);
-		System.out.println(personBean.getName());
-		System.out.println(personBean.getComputer().getModel());
+	//	Person personBean= a.getBean("myPerson1",Person.class);
+	//	System.out.println(personBean.getName());
+	//	System.out.println(personBean.getComputer().getModel());
 		
-		Employee employeeBean=a.getBean("myEmployee2",Employee.class);
-		System.out.println(employeeBean.getName());
-		System.out.println(employeeBean.getComputer().getColor());
+	//	Employee employeeBean=a.getBean("myEmployee2",Employee.class);
+	//	System.out.println(employeeBean.getName());
+	//	System.out.println(employeeBean.getComputer().getColor());
 		
-		Home homeBean = a.getBean(Home.class);
+	//	Home homeBean = a.getBean(Home.class);
 		
-		Home homeBean2 = a.getBean(Home.class);
+	//	Home homeBean2 = a.getBean(Home.class);
 		//singleton
 		//prototype
 		
